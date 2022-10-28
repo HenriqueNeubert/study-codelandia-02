f282dd9265f335d3e885c29572049a9ab114156f
HenriqueNeubert <neubert.study@gmail.com>
2022-10-28 11:16:34 -0300

    feat: created footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 import React from "react";
 import Banner from "./components/banner/Banner";
-import HeaderTop from "./components/header/HeaderTop"
-import HeaderBottom from "./components/header/HeaderBottom"
+import Footer from "./components/footer/Footer";
+import HeaderTop from "./components/header/HeaderTop";
+import HeaderBottom from "./components/header/HeaderBottom";
 
 function App() {
-  return <>
-    <header>
-      <HeaderTop
-        title="Ganhe R$ 10,00 de desconto no frete" 
-        background='#212529'
-        color='#fff'
-        align="center"
-      />
-      <HeaderBottom
-        title="JordanShoes" 
-        background='#fff'
-        color='#212529'
-        align="center" 
-      />
-    </header>
-    <Banner
-      color='#fff'
-      title="A melhor loja de Jordan"
-      description="O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."
-      alignX="end"
-      alignY="center"
-    >
-
-    </Banner>
-  </>;
+  return (
+    <>
+      <header>
+        <HeaderTop
+          title="Ganhe R$ 10,00 de desconto no frete"
+          background="#212529"
+          color="#fff"
+          align="center"
+        />
+        <HeaderBottom
+          title="JordanShoes"
+          background="#fff"
+          color="#212529"
+          align="center"
+        />
+      </header>
+      <Banner
+        color="#fff"
+        title="A melhor loja de Jordan"
+        description="O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."
+        alignX="end"
+        alignY="center"
+      ></Banner>
+      <Footer
+        title="Todos os direitos reservados."
+        background="#212529"
+        color="#fff"
+        align="start"></Footer>
+    </>
+  );
 }
 
 export default App;
